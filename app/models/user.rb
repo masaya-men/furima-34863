6bb205cdd36b33969185b4419487e9f6f5bce7b6
masaya-men <79306361+masaya-men@users.noreply.github.com>
2021-03-20 16:53:29 +0900

    Userモデル単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,33 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/} do
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ　ー－]+\z/} do
+    validates :last_furigana
+    validates :first_furigana
+  end
+  validates :birthday, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+
 end
+
+# 日本語で出力しようとした
+# validates :nickname, presence: {message: 'を入力してください'}
+# with_options presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/, message: 'お名前（全角）の入力には、全角の漢字・ひらがな・カタカナのみが使用可能です'} do
+#   validates :last_name
+#   validates :first_name
+# end
+# with_options presence: true, format: { with: /\A[ァ-ヶ　ー－]+\z/, message: 'お名前カナ（全角）の入力には、全角のカタカナのみが使用可能です'} do
+#   validates :last_furigana
+#   validates :first_furigana
+# end
+# validates :birthday, presence: {message: 'を入力してください'}
+
+# PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+# validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角英字と半角数字の両方を含めて設定してください'
+# end
