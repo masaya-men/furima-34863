cb10b52db0fc49e70e0ba77bf29ecb938a08a045
masaya-men <79306361+masaya-men@users.noreply.github.com>
2021-03-23 19:44:37 +0900

    商品情報編集機能_1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  # before_action :item_params, only: :show
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :set_params, only: [:show, :edit]
+  before_action :item_params, only: :update
 
   def index
     @items = Item.order("created_at DESC")
@@ -20,7 +21,23 @@ class ItemsController < ApplicationController
   end
 
   def show
+    # @item = Item.find(params[:id])LGTM後に消す
+  end
+
+  def edit
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
+    # @item = Item.find(params[:id])
+  end
+
+  def update
     @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(params[:id])
+    else
+      render :edit
+    end
   end
 
   private
@@ -28,5 +45,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(
       :item_name, :item_detail, :status_id, :category_id, :price, :shipping_charge_id, :shipping_day_id, :prefecture_id, :image
     ).merge(user_id: current_user.id)
+  end
+
+  def set_params
+    @item = Item.find(params[:id])
   end
 end
